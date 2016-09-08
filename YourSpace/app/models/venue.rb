7fa35ebd9a59849062081267484c58aa39ee6b89
jaspercurry <jaspercurry@gmail.com>
2016-09-08 10:34:19 -0400

    delete nested folder

--- YourSpace/app/models/venue.rb
+++
@@ -1,8 +0,0 @@
-class Venue < ApplicationRecord
-  has_many :listings
-  has_many :reservations, through: :listings
-  has_many :renters, through: :reservations, :foreign_key => "renter_id"
-  belongs_to :host, :class_name => "User"
-  # has_many :reviews, through: :listings
-
-end
