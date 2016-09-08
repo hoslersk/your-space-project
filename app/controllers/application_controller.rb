c4620141684e136b4b82b96fd09aa1b141190b31
Silvana Goberdhan-Vigle <silvanagoberdhanvigle@gmail.com>
2016-09-08 18:03:54 -0400

    added app controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def logged_in?
+
+  end
+
+  def authenticate
+  end
+
+  def authorize
+  end
+
+  def current_user
+  end
 end
