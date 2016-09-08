2b9b2cb3f2d009711ad87e788a4e6828402a6b06
Schyler Hosler <schylerhosler@gmail.com>
2016-09-08 11:48:21 -0400

    This update will enable a home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
 
     before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+    def home
+    end
+
     def index
       @users = User.all
     end
