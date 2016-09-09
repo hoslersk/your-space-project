61f61187a91305fbb01de003dd8747f612d55542
Schyler Hosler <schylerhosler@gmail.com>
2016-09-09 13:42:53 -0400

    This update contains initial set-up for email functionality/connectivity. Incomplete.

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,4 +1,5 @@
-class ApplicationMailer < ActionMailer::Base
+class NotificationMailer < ApplicationMailer
+
   default from: 'schylerhosler@gmail.com'
   layout 'mailer'
 
@@ -6,4 +7,7 @@ class ApplicationMailer < ActionMailer::Base
     @user = user
     mail(to: @user.email, subject: 'Sample Email')
   end
+
+  # NotificationMailer.notification_email(@user).deliver #for use in UsersController
+
 end
