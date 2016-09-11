077906aee96cf8e2a00d31d28c70e75d95edac64
vronnieli <vronnieli@gmail.com>
2016-09-11 19:47:53 -0400

    added default image

--- app/models/image.rb
+++ app/models/image.rb
@@ -5,7 +5,7 @@ class Image < ApplicationRecord
   validates :image, presence: {message: 'Please upload an image'}
   validates :description, presence: {message: 'Please enter a description'}
 
-  has_attached_file :image, styles: { small: "300x300", medium: "475x475" }
+  has_attached_file :image, :default_url => "/images/missing.png", styles: { small: "300x300", medium: "475x475" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
