579b615e0e2257f1ebf666c0f27c411be003fdb5
jaspercurry <jaspercurry@gmail.com>
2016-09-13 01:06:19 -0400

    Modifies calendar view.

--- app/assets/javascripts/host_reservations.js
+++ app/assets/javascripts/host_reservations.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
   $('#calendar_res_h').fullCalendar({
-    events: '/host_reservations.json'
+    events: '/host_reservations.json',
+    eventRender: function(event, element, view) {
+      if (event.description == "pending") {
+        //apply your logic here, make changes to element.
+        element.css('border', 'blue')
+        element.css('background', 'blue');
+      } else if (event.description == "confirmed") {
+        element.css('border', 'green')
+        element.css('background', 'green');
+      }
+    }
   });
 });
